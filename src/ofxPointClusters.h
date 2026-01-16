f04fece3e51041c2a7dc2fb504286051d3fffb0a
smeyfroi <steve.meyfroidt@gmail.com>
2026-01-16 15:03:35 +0000

    Increase max clusters

--- src/ofxPointClusters.h
+++ src/ofxPointClusters.h
@@ -27,7 +27,7 @@ public:
   std::string getParameterGroupName() const { return "Point Clusters"; }
   ofParameterGroup parameters;
   ofParameter<int> maxSourcePointsParameter { "Max Source Points", 2000, 500, 96000 }; // Note: we only use "valid" samples
-  ofParameter<float> clustersParameter { "Clusters", 5.0, 2.0, 11.0 };
+  ofParameter<float> clustersParameter { "Clusters", 5.0, 2.0, 17.0 };
   ofParameterGroup& getParameterGroup();
 
 protected:
